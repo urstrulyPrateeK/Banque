8095936fced5c345dff2969ab7adc63478c50db2
Prateek Singh <129511457+urstrulyPrateeK@users.noreply.github.com>
2026-04-12 05:00:51 +0530

    backend updated

---
+++ bank-backend/src/main/java/com/bank/bankbackend/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.bank.bankbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1/home")
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "Hello World";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "Hello Admin";
+    }
+
+    @GetMapping("/customer")
+    public String customer(){
+        return "Hello Customer";
+    }
+
+
+    @GetMapping("/ott/sent")
+    public  String ottSent(){
+        return "OTP sent";
+    }
+
+}
